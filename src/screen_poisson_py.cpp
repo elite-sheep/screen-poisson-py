ab6744f77ff73063fec860bf00ea2c0fc445a562
Yu-Chen Wang <yuchew23@uci.edu>
2025-01-10 18:22:17 -0800

    fix_bug

--- src/screen_poisson_py.cpp
+++ src/screen_poisson_py.cpp
@@ -8,5 +8,10 @@ using namespace nb::literals;
 NB_MODULE(screenpoissonpy, m) {
     nb::class_<poisson::SolverParams>(std::move(m), "PoissonParams")
         .def(nb::init<>())
-        .def("setConfigPreset", &poisson::SolverParams::setConfigPreset);
+        .def("setConfigPreset", &poisson::SolverParams::setConfigPreset)
+        .def_rw("brightness", &poisson::SolverParams::brightness);
+
+    nb::class_<poisson::Solver>(std::move(m), "PoissonSolver")
+        .def(nb::init<poisson::SolverParams>())
+        .def("solve", &poisson::Solver::solveIndirect);
 }

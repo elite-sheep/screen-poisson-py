2addf7bd86617d2a32063bf24e263b873e120c90
Yu-Chen Wang <yuchew23@uci.edu>
2025-01-10 17:40:09 -0800

    commit_1

--- src/screen_poisson_py.cpp
+++ src/screen_poisson_py.cpp
@@ -1,9 +1,12 @@
 #include <nanobind/nanobind.h>
 
-int add(int a, int b) {
-    return a + b;
-}
+#include "Solver.hpp"
 
-NB_MODULE(screen_poisson_py, m) {
-    m.def("add", &add);
+namespace nb = nanobind;
+using namespace nb::literals;
+
+NB_MODULE(screenpoissonpy, m) {
+    nb::class_<poisson::SolverParams>(std::move(m), "PoissonParams")
+        .def(nb::init<>())
+        .def("setConfigPreset", &poisson::SolverParams::setConfigPreset);
 }
